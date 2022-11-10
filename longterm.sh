e954122af9622e88f53151a206bbf3008cece92e
WinSSitaly <giglione@libero.it>
2022-11-10 11:59:40 +0100

    longterm 5.10.152

--- longterm.sh
+++ longterm.sh
@@ -24,21 +24,10 @@ fi
 #git clone https://github.com/microsoft/WSL2-Linux-Kernel
 #git clone https://github.com/nathanchance/WSL2-Linux-Kernel
 
-if [[ $versione == *"rc"* ]]; then
-echo "Compilo la versione release candidate linux-$versione!"
-wget https://git.kernel.org/torvalds/t/linux-$versione.tar.gz
-tar -xzvf linux-$versione.tar.gz
-wget https://github.com/WinSSitaly/WSL-Linux-Kernel/raw/main/mywsl-configrc -O .config
-#cp $DIR/WSL2-Linux-Kernel/.configrc $DIR/linux-$versione/.config
-else
-echo "Compilo la versione stabile linux-$versione!"
+echo "Compilo la versione longterm linux-$versione!"
 wget https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-$versione.tar.xz
 tar -xf linux-$versione.tar.xz
-#cp $DIR/WSL2-Linux-Kernel/.config $DIR/linux-$versione/
-#wget https://github.com/WinSSitaly/WSL-Linux-Kernel/raw/main/mywsl-config -O .config
-#wget https://raw.githubusercontent.com/WinSSitaly/WSL2-Linux-Kernel/linux-msft-wsl-5.10.y/Microsoft/config-wsl
-wget https://github.com/WinSSitaly/WSL2-Linux-Kernel/raw/linux-msft-wsl-5.10.y/Microsoft/config-wsl -O .config
-fi
+wget https://github.com/WinSSitaly/WSL-Linux-Kernel/raw/main/mywsl-configlt -O .config
 
 searchDir="/mnt/c/Users/"
 Utenti=()
@@ -57,26 +46,21 @@ if [[ ${#Utenti[@]} -ne 0 ]]; then
 	fi
 	done
 fi
+
+
 cd $DIR
 cd linux-$versione
 cp ../.config ./
 make clean
-make KCONFIG_CONFIG=.config -j8
+make KCONFIG_CONFIG=.config -j256
 cp arch/x86/boot/bzImage /mnt/c/wslconfig/bzImage_$versione
 cp .config ../
-if [[ $versione == *"rc"* ]]; then
-	cp .config /mnt/c/wslconfig/mywsl-configrc
-else
-	cp .config /mnt/c/wslconfig/mywsl-config
-fi
+cp .config /mnt/c/wslconfig/mywsl-configlt
 
-cp .config /mnt/c/wslconfig/
 cp /mnt/c/wslconfig/bzImage_$versione /mnt/c/wslconfig/bzImage-test
 
 cp .config /mnt/c/wslconfig/mywsl-configlt
 cp .config $DIRgh/WSL-Linux-Kernel/mywsl-configlt
-
-
 
 cd $DIRgh/WSL-Linux-Kernel/
 make git m="longterm $versione"
