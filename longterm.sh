f876c0cb27ad70cef90a999d87d413552fc9efb8
WinSSitaly <giglione@libero.it>
2023-01-20 08:53:53 +0100

    stable 6.1.7

--- longterm.sh
+++ longterm.sh
@@ -4,7 +4,7 @@ DIR="$PWD"
 DIRgh=$DIR/ciao
 
 if [ -d "$DIRWSL" ]; then
-echo $DIRWSL esiste
+echo $DIRWSL exist
 else
 mkdir $DIRWSL
 fi
@@ -12,21 +12,16 @@ cd $DIR
 wget https://github.com/WinSSitaly/WSL-Linux-Kernel/raw/main/.wslconfig -O .wslconfig
 #wget https://github.com/WinSSitaly/WSL-Linux-Kernel/.wslconfig
 cp .wslconfig /mnt/c/wslconfig/.wslconfig
-read -p "Enter version number :  " versione
+read -p "Enter version number :  " version
 if [ -f "/mnt/c/wslconfig/bzImage-test" ]; then
-mv /mnt/c/wslconfig/bzImage-test /mnt/c/wslconfig/bzImage_pre-$versione
+mv /mnt/c/wslconfig/bzImage-test /mnt/c/wslconfig/bzImage_pre-$version
 else
-echo "non esiste bzImage-test"
+echo "bzImage-test not exist"
 fi
 
-#versione=5.12_latest
-#scarica official wsl2
-#git clone https://github.com/microsoft/WSL2-Linux-Kernel
-#git clone https://github.com/nathanchance/WSL2-Linux-Kernel
-
-echo "Compilo la versione longterm linux-$versione!"
-wget https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-$versione.tar.xz
-tar -xf linux-$versione.tar.xz
+echo "Compiling longterm version linux-$version!"
+wget https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-$version.tar.xz
+tar -xf linux-$version.tar.xz
 wget https://github.com/WinSSitaly/WSL-Linux-Kernel/raw/main/mywsl-configlt -O .config
 
 searchDir="/mnt/c/Users/"
@@ -49,20 +44,21 @@ fi
 
 
 cd $DIR
-cd linux-$versione
+cd linux-$version
 cp ../.config ./
 make clean
-make KCONFIG_CONFIG=.config -j256
-cp arch/x86/boot/bzImage /mnt/c/wslconfig/bzImage_$versione
+make KCONFIG_CONFIG=.config -j128
+cp arch/x86/boot/bzImage /mnt/c/wslconfig/bzImage_$version
 cp .config ../
 cp .config /mnt/c/wslconfig/mywsl-configlt
 
-cp /mnt/c/wslconfig/bzImage_$versione /mnt/c/wslconfig/bzImage-test
+cp /mnt/c/wslconfig/bzImage_$version /mnt/c/wslconfig/bzImage-test
 
+cp .config /mnt/c/wslconfig/mywsl-configlt
 cp .config $DIRgh/WSL-Linux-Kernel/mywsl-configlt
 
 cd $DIRgh/WSL-Linux-Kernel/
-make git m="longterm $versione"
+make git m="longterm $version"
 
 cd $DIR
 
